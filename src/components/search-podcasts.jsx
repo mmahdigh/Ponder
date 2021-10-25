e10d928ce754776d128156b95d969236905d5061
Matt Palmer <mspalmer91@gmail.com>
2021-10-25 12:32:37 -0400

    search button improvements

--- src/components/search-podcasts.jsx
+++ src/components/search-podcasts.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { Formik, Form as FormikForm, Field } from 'formik';
 import { InputGroup, Form, Button } from 'react-bootstrap';
 import * as Yup from 'yup';
-import { FaSearch } from 'react-icons/fa';
+import { FaRss, FaPlus } from 'react-icons/fa';
+
+const SearchButton = styled(InputGroup.Text)`
+  padding-top: 0;
+  padding-right: 0;
+  padding-bottom: 0;
+  padding-left: .2em;
+
+  button {
+    margin: 0;
+    border: 0;
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+  }
+`;
+
+const PlusIcon = styled(FaPlus)`
+  position: absolute;
+  right: 4px;
+  top: 4px;
+  font-size: .7rem;
+`;
 
 const validationSchema = Yup.object().shape({
   query: Yup.string().url().required().trim(),
@@ -23,12 +45,13 @@ function SearchPodcasts({ onSubmit }) {
           {console.log(errors)}
           <Form.Group controlId="query">
             <InputGroup>
-              <Field name="query" disabled={submitting} />
-              <InputGroup.Text>
+              <Field name="query" disabled={submitting} placeholder="RSS URL" />
+              <SearchButton>
                 <Button type="submit">
-                  <FaSearch />
+                  <FaRss />
+                  <PlusIcon />
                 </Button>
-              </InputGroup.Text>
+              </SearchButton>
             </InputGroup>
           </Form.Group>
         </FormikForm>
