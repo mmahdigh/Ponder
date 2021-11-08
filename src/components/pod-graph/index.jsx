fff0d1a78501ef97fe7058846347393574e73ae6
Matthew Palmer <matthew.palmer1@mohawkcollege.ca>
2021-11-08 11:09:39 -0500

    Pod Graph Data Source (#3)
    * transform nodes and edges
    
    * start rendering elements from real data
    
    * element processing
    
    * stuff
    
    Co-authored-by: Matt Palmer <mspalmer91@gmail.com>

--- src/components/pod-graph/index.jsx
+++ src/components/pod-graph/index.jsx
@@ -1,20 +1,51 @@
 import React, { useContext, useState } from 'react';
-import PropTypes from 'prop-types';
 import Cytoscape from 'react-cytoscapejs';
 import applyCytoscape from './cytoscape';
 import { CytoscapeContext } from '../../providers/cytoscape';
-import normalizeData from './normalize-data';
 import layout from './layout';
 import styles from './styles';
 import Legend from './legend';
+import { SubscriptionsContext } from '../../providers/subscriptions';
 import PodcastDetails from '../podcast-details';
-import { podcastPropType } from '../../prop-types';
 
-function PodGraph({ subscriptions }) {
+function PodGraph() {
   const { setCytoscape } = useContext(CytoscapeContext);
-  const [isDetailsOpen, setIsDetailsOpen] = useState(false);
+  const { subscriptions } = useContext(SubscriptionsContext);
   const [selectedPodcast, setSelectedPodcast] = useState(null);
-  const elements = Cytoscape.normalizeElements(normalizeData());
+
+  const elements = Cytoscape.normalizeElements({
+    nodes: subscriptions.map(({ episodes, ...podcast }) => ({
+      data: {
+        id: podcast.subscribeUrl,
+        label: podcast.title,
+        categories: podcast.categories.join(',\n'),
+        bgImg: podcast.imageUrl,
+        NodesBg: 'green', // TODO: Make 'grey' if not subscribed podcast
+      },
+    })),
+    edges: subscriptions.reduce((acc, podcast, i, xs) => {
+      // A match is any other podcast that has one same category or keyword
+      const matches = xs.filter(({ categories, keywords }) => categories
+        .some(category => podcast.categories.includes(category))
+        || keywords.some(keyword => podcast.keywords.includes(keyword)));
+
+      // If there are no matches there is nothing to add
+      if (!matches.length) return acc;
+
+      // Remove duplicates
+      // matches = matches.filter((match, i, xs) => xs.map(a ));
+
+      // Tack dat on
+      return acc.concat(matches.map(match => ({
+        source: podcast.subscribeUrl,
+        target: match.subscribeUrl,
+        label: podcast.categories.filter(category => match.categories.includes(category))
+          .concat(podcast.keywords.filter(keyword => match.keywords.includes(keyword)))
+          .join(', '),
+        isMatch: true, // There will be three different edge styles
+      })));
+    }, []),
+  });
 
   return (
     <>
@@ -28,21 +59,13 @@ function PodGraph({ subscriptions }) {
         style={{
           minWidth: '100%',
           minHeight: 600,
-          backgroundCOlor: '#202022',
+          backgroundColor: '#202022',
         }}
       />
       <Legend />
-      <PodcastDetails isOpen={isDetailsOpen} close={() => setIsDetailsOpen(false)} />
+      <PodcastDetails isOpen={!!selectedPodcast} close={() => setSelectedPodcast(null)} />
     </>
   );
 }
-
-PodGraph.propTypes = {
-  subscriptions: PropTypes.arrayOf(podcastPropType),
-};
-
-PodGraph.defaultProps = {
-  subscriptions: [],
-};
 
 export default PodGraph;
