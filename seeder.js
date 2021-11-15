a6ae6acd4a3c3afc675d777b3dc74eeca024e5c9
Matt Palmer <mspalmer91@gmail.com>
2021-11-15 11:10:37 -0500

    add seeder logic

---
+++ seeder.js
@@ -0,0 +1,31 @@
+'use strict';
+
+require('dotenv').config();
+const Arweave = require('arweave');
+const key = require('./src/client/arweave/key.json');
+const seeds = require('./seeds.json');
+
+(async () => {
+  const client = Arweave.init({
+    host: 'localhost',
+    port: 1984,
+    protocol: 'http',
+    timeout: 20000,
+    logging: true,
+  });
+
+  console.log('Begin seeding...');
+  await Promise.all(seeds
+    .map(({ contents, tags }) => client.createTransaction({ data: JSON.stringify(contents) }, key)
+      .then(trx => {
+        trx.addTag('Content-Type', 'application/json');
+        trx.addTag('Unix-Time', Math.floor(Date.now() / 1000));
+        trx.addTag(`${process.env.TAG_PREFIX}-version`, process.env.VERSION);
+        tags.forEach(([k, v]) => {
+          trx.addTag(k, v);
+        });
+        return client.transactions.sign(trx, key)
+          .then(() => client.transactions.post(trx));
+      })));
+  console.log('Seeding successful!');
+})();
