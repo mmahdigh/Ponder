8fa887d56dd61265a6820825965a6c40072a8eb3
Matt Palmer <mspalmer91@gmail.com>
2021-11-15 15:05:56 -0500

    finish basic seeder

--- seeder.js
+++ seeder.js
@@ -5,7 +5,12 @@ const Arweave = require('arweave');
 const key = require('./src/client/arweave/key.json');
 const seeds = require('./seeds.json');
 
-(async () => {
+const TAG_MAP = {
+  categories: 'category',
+  keywords: 'keyword',
+};
+
+async function seeder() {
   const client = Arweave.init({
     host: 'localhost',
     port: 1984,
@@ -21,11 +26,20 @@ const seeds = require('./seeds.json');
         trx.addTag('Content-Type', 'application/json');
         trx.addTag('Unix-Time', Math.floor(Date.now() / 1000));
         trx.addTag(`${process.env.TAG_PREFIX}-version`, process.env.VERSION);
-        tags.forEach(([k, v]) => {
-          trx.addTag(`${process.env.TAG_PREFIX}-${k}`, v);
-        });
+        Object.entries(tags)
+          .reduce(
+            (acc, [k, v]) => acc.concat(['categories', 'keywords'].includes(k)
+              ? v.map(a => [TAG_MAP[k], a])
+              : [[k, v]]),
+            [],
+          )
+          .forEach(([k, v]) => {
+            trx.addTag(`${process.env.TAG_PREFIX}-${k}`, v);
+          });
         return client.transactions.sign(trx, key)
           .then(() => client.transactions.post(trx));
       })));
   console.log('Seeding successful!');
-})();
+}
+
+setTimeout(seeder, 5000);
