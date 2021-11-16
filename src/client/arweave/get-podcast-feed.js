daef2ae6e44ddf55e5cebe9a64e94664aca40684
Matt Palmer <mspalmer91@gmail.com>
2021-11-16 12:08:58 -0500

    parse arweave response

--- src/client/arweave/get-podcast-feed.js
+++ src/client/arweave/get-podcast-feed.js
@@ -1,6 +1,6 @@
 import gql from 'fake-tag';
 import client from './client';
-import { toTag } from './utils';
+import { toTag, fromTag } from './utils';
 
 export default async function getPodcastFeed(subscribeUrls) {
   const transactions = await client.api.post('/graphql', {
@@ -20,21 +20,39 @@ export default async function getPodcastFeed(subscribeUrls) {
       }
     `,
     variables: {
-      podcastTags: [
+      tags: [
         {
           name: toTag('subscribeUrl'),
-          values: subscribeUrls,
-        },
-        {
-          name: toTag('podcastSubscribeUrl'),
           values: subscribeUrls,
         },
       ],
     },
   })
-    .then(res => res.data.data.podcast.edges.map(edge => edge.node));
+    .then(res => res.data.data.transactions.edges.map(edge => edge.node));
 
-  console.log(transactions);
-
-  return transactions;
+  return Promise.all(transactions.map(transaction => client.transactions.getData(transaction.id, {
+    decode: true,
+    string: true,
+  })
+    .then(JSON.parse)
+    .then(podcast => ({
+      ...podcast,
+      ...transaction.tags
+        .map(tag => ({
+          ...tag,
+          name: fromTag(tag.name),
+        }))
+        .filter(tag => !['Content-Type', 'Unix-Time', 'version'].includes(tag.name))
+        .reduce((acc, tag) => ({
+          ...acc,
+          [tag.name]: Array.isArray(acc[tag.name]) ? acc[tag.name].concat(tag.value) : tag.value,
+        }), {
+          categories: [],
+          keywords: [],
+        }),
+      episodes: podcast.episodes.map(episode => ({
+        ...episode,
+        publishedAt: new Date(episode.publishedAt),
+      })),
+    }))));
 }
