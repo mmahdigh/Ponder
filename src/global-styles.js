88db4169bcb5928f30cfa19d7731c72f43183ef6
kagwi andrew <kagwiandrew@gmail.com>
2021-11-16 12:13:28 +0300

    panzoom custom style

--- src/global-styles.js
+++ src/global-styles.js
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
+import 'cytoscape-panzoom/cytoscape.js-panzoom.css';
 
 export default createGlobalStyle`
   :root {
@@ -15,4 +16,29 @@ export default createGlobalStyle`
   body {
     background-color: var(--color-body);
   }
+
+  // panzoom controller
+.cy-panzoom{
+    top: 1rem;
+      left: 1rem;
+      color: #666;
+}
+.cy-panzoom-panner {
+    background: #121213;
+    border: 1px solid #23476d;
+}
+.cy-panzoom-slider-background {
+    background: #121213;
+    border-left: 1px solid #23476d;
+    border-right: 1px solid #23476d;
+}
+.cy-panzoom-slider-handle {
+    background: #121213;
+    border: 1px solid #23476d;
+}
+.cy-panzoom-zoom-button {
+    background: #121213;
+    border: 1px solid #23476d;
+}
+
 `;
