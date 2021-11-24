b4bcada896439dd81ef2de596c7e85b56be3413f
Matt Palmer <mspalmer91@gmail.com>
2021-11-24 15:38:52 -0500

    finish moving over andrews changes

--- src/components/cytoscape/cytoscape/index.js
+++ src/components/cytoscape/cytoscape/index.js
@@ -1,9 +1,12 @@
 import cytoscape from 'cytoscape';
+import dagre from 'cytoscape-dagre';
 import layout from './layout';
 import styles from './styles';
 import applyPanzoom from './panzoom';
 import applyEvents from './events';
 import applyNodeGroups from './node-groups';
+
+cytoscape.use(dagre);
 
 export default function createCytoscape(container, elements, deps) {
   const cy = cytoscape({
