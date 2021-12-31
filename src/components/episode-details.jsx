a41eb68405f0db437019fbc4fe611404bf688714
kagwi andrew <kagwiandrew@gmail.com>
2021-12-31 21:08:45 +0300

    added the 2-spaces tab in my styling

--- src/components/episode-details.jsx
+++ src/components/episode-details.jsx
@@ -7,79 +7,79 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 import { MdMoreTime, MdOutlineCloudUpload } from 'react-icons/md';
 
 const EpisodeLink = styled.a`
-    color: #9c8f94;
-    padding: 0;
-    margin-bottom: 0.25rem;
-    display: block;
-    text-decoration: none;
-    &:hover,
-    &:focus {
-        color: palevioletred;
-    }
-    &:active {
-        color: red;
-    }
+  color: #9c8f94;
+  padding: 0;
+  margin-bottom: 0.25rem;
+  display: block;
+  text-decoration: none;
+  &:hover,
+  &:focus {
+    color: palevioletred;
+  }
+  &:active {
+    color: red;
+  }
 `;
 const DetailsCard = styled.div`
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    min-width: 0;
-    word-wrap: break-word;
-    background-color: #181819;
-    background-clip: border-box;
-    border: 1px solid rgba(47, 42, 48, 0.85);
-    border-radius: 0.375rem;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  min-width: 0;
+  word-wrap: break-word;
+  background-color: #181819;
+  background-clip: border-box;
+  border: 1px solid rgba(47, 42, 48, 0.85);
+  border-radius: 0.375rem;
 `;
 
 const CardBody = styled.div`
-    padding: 0.5rem 1.25rem;
-    display: flex;
-    min-height: 1px;
+  padding: 0.5rem 1.25rem;
+  display: flex;
+  min-height: 1px;
 `;
 const EpisodeImage = styled.div`
-    display: inline-block;
-    position: relative;
-    line-height: 0;
-    margin-right: 1rem;
+  display: inline-block;
+  position: relative;
+  line-height: 0;
+  margin-right: 1rem;
 `;
 const Content = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-    width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  width: 100%;
 
 `;
 // styling the episode image
 const EpisodeImageStyle = styled(Image)`
-    height: 3.125rem;
-    min-height: 3.125rem;
-    width: 3.125rem;
-    min-width: 3.125rem;
-    border-radius: 50%;
+  height: 3.125rem;
+  min-height: 3.125rem;
+  width: 3.125rem;
+  min-width: 3.125rem;
+  border-radius: 50%;
 `;
 const TimeBadge = styled.small`
-    display: flex;
-    align-items: center;
-    line-height: 1;
-    font-size: xx-small;
-    color: #9c8f94;
+  display: flex;
+  align-items: center;
+  line-height: 1;
+  font-size: xx-small;
+  color: #9c8f94;
 `;
 const TimeIcon = styled(MdMoreTime)`
-    font-size: 12px;
-    margin: 2px;
+  font-size: 12px;
+  margin: 2px;
 `;
 const CalenderIcon = styled(MdOutlineCloudUpload)`
-    font-size: 12px;
-    margin: 3px;
+  font-size: 12px;
+  margin: 3px;
 `;
 const PodcastDetails = styled.div`
-    display: flex;
-    align-items: end;
-    line-height: 1;
-    font-size: xx-small;
-    color: #9c8f94;
-    margin-top: 1px;
+  display: flex;
+  align-items: end;
+  line-height: 1;
+  font-size: xx-small;
+  color: #9c8f94;
+  margin-top: 1px;
 `;
 dayjs.extend(relativeTime);
 function EpisodeDetails({
--- src/components/remove-button.jsx
+++ src/components/remove-button.jsx
@@ -4,25 +4,23 @@ import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
 import { CgTrash } from 'react-icons/cg';
 
-
 const MinusIcon = styled(CgTrash)`
   font-size: 1.25rem;
-    line-height: 1.75rem;
+  line-height: 1.75rem;
 `;
 const DeleteBtn = styled(Button)`
-    padding: 0.25rem 0.5rem;
-    line-height: 1.75rem;
-    border-radius: 50%;
-    background: transparent;
-    border: 1px solid transparent;
-    color: #bd0c0c ;
-    box-shadow: none !important;
-    &:hover {
+  padding: 0.25rem 0.5rem;
+  line-height: 1.75rem;
+  border-radius: 50%;
+  background: transparent;
+  border: 1px solid transparent;
+  color: #bd0c0c ;
+  box-shadow: none !important;
+  &:hover {
     color: #641c0f;
     background: transparent;
     border: 1px solid transparent;
 }
-
 `;
 
 function RemoveBtn({
